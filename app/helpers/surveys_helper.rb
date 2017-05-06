bad1f3db87667e45622eda89ba5bbc5bd87277d6
Chris Laskey <contact@chrislaskey.com>
2017-05-06 10:39:27 -0400

    Rubocop fix

--- app/helpers/surveys_helper.rb
+++ app/helpers/surveys_helper.rb
@@ -1,7 +1,5 @@
 module SurveysHelper
-
   def element(type, name, label, current: nil)
     render partial: "surveys/elements/#{type}", locals: { name: name, label: label, current: current }
   end
-
 end
